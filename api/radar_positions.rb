8bd253030112416a0b1a104da16a0b4f1c0b9f0a
Julio Antequera Galiano <julio.antequera.galiano@gmail.com>
2016-09-27 10:55:25 +0200

    Removes invalid (empty) positions after on initialization (test 6)

--- api/radar_positions.rb
+++ api/radar_positions.rb
@@ -3,6 +3,7 @@ class RadarPositions
 
   def initialize(positions)
     @positions = positions.collect{ |rp| RadarPosition.new(rp) }
+    @positions = @positions.select{ |t| t.valid? }
     @positions = @positions.sort{|p1, p2| p1.distance <=> p2.distance}
   end
 
